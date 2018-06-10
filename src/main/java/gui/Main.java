75681ab8c1201617472b817d8e31a35dda31e24f
Doug <doug.paz@hotmail.com>
2018-06-10 17:37:54 -0300

    Ajustes criacao EXE

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) throws Exception {
 
 		primaryStage.setTitle("My-Flight");
-		primaryStage.getIcons().add(new Image("file:src/main/resources/imagens/logo.png"));
+		primaryStage.getIcons().add(new Image("file:src/main/resources/imagens/Logo.png"));
 		Parent root = FXMLLoader.load(getClass().getResource("/Main.fxml"));
 		primaryStage.setScene(new Scene(root));
 		primaryStage.show();
